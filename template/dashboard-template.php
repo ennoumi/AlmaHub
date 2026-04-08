477c0cf8e2af5dc1684e171c7ed4a24688144314
maisam <maisam.noumi@studio.unibo.it>
2026-04-09 00:09:43 +0200

    Added admin link to dashboard

--- template/dashboard-template.php
+++ template/dashboard-template.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <?php if (isAdmin()): ?>
+        <p><a href="admin.php">Pannello Admin</a></p>
+    <?php endif; ?>
 </head>
 <body>
     <h1><?php echo "Bentornato, " . $_SESSION['utente']['nome'];?></h1>
