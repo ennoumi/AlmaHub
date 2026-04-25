e934fc472e217c504c47e834f9a89a070e874ef9
maisam <maisam.noumi@studio.unibo.it>
2026-04-25 13:12:53 +0200

    Fixed header.php

--- template/layout/header.php
+++ template/layout/header.php
@@ -1,18 +1,23 @@
 <header>
-    <nav class="navbar">
-        <div class="logo">
-            <a href="index.php"><strong>AlmaHub</strong></a>
-        </div>
-        <ul class="nav-links">
-            <?php if (isset($_SESSION['utente'])): ?>
-                <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="group-requests.php">Richieste</a></li>
-                <li><a href="profile.php"><?php echo $_SESSION['utente']['nome'] . ' ' . $_SESSION['utente']['cognome']; ?></a></li>
-                <li><a href="logout.php" class="btn-logout">Logout</a></li>
-            <?php else: ?>
-                <li><a href="login.php">Accedi</a></li>
-                <li><a href="register.php">Registrati</a></li>
-            <?php endif; ?>
-        </ul>
-    </nav>
+<nav>
+    <div>
+        <a href="/AlmaHub/public/index.php"><strong>AlmaHub</strong></a>
+    </div>
+    <ul>
+        <?php if (isset($_SESSION['utente'])): ?>
+          <li>
+              <a href="/AlmaHub/public/profile.php">
+                  <?php echo $_SESSION['utente']['nome'] . ' ' . $_SESSION['utente']['cognome']; ?>
+              </a>
+          </li>
+
+          <li><a href="/AlmaHub/public/dashboard.php">Dashboard</a></li>
+          <li><a href="/AlmaHub/public/group-requests.php">Richieste di Partecipazione</a></li>
+          <li><a href="/AlmaHub/public/logout.php">Logout</a></li>
+        <?php else: ?>
+          <li><a href="/AlmaHub/public/login.php">Accedi</a></li>
+          <li><a href="/AlmaHub/public/register.php">Registrati</a></li>
+        <?php endif; ?>
+    </ul>
+</nav>
 </header>
