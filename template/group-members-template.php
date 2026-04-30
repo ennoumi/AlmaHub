3c681d37980d8089ec2d3636cac505b17e8bcbde
riem_boukhama <riem.boukhama@studio.unibo.it>
2026-04-30 15:49:41 +0200

    added styling for group members page

--- template/group-members-template.php
+++ template/group-members-template.php
@@ -4,22 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Membri</title>
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/group-members.css">
 </head>
 <body>
     <?php include 'layout/header.php'; ?>
-    <h1>Membri di: <?php echo $gruppo['titolo']?></h1>
-        <p><a href="group-details.php?id=<?php echo $idGruppo; ?>"> Torna al Gruppo</a></p>
+
+    <div class="group-members-body">
+        <h1 class="gradient-text">Membri di <?php echo $gruppo['titolo']?></h1>
+        <p class="dashboard-link"><a href="group-details.php?id=<?php echo $idGruppo; ?>"> Torna al Gruppo</a></p>
+        <hr>
         <ul>
             <?php foreach($templateParams["membri"] as $m):?>
             <li>
-                <p><?php echo $m["nome"]?></p>
-                <p><?php echo $m["cognome"];?></p>
+                <p class="member-type <?php echo $m["ruolo"];?>"><?php echo $m["ruolo"];?></p>
+                <p class="name"><?php echo $m["nome"] . ' ' . $m["cognome"];?></p>
                 <p><?php echo $m["email"];?></p>
-                <p><?php echo $m["ruolo"];?></p>
-                <a href="profile.php?user=<?php echo $m['id_utente'];?>">Profilo</a>
+                <p class="profile-link btn btn-secondary"><a href="profile.php?user=<?php echo $m['id_utente'];?>">Profilo</a></p>
+                <hr class="member-separator">
             </li>
             <?php endforeach;?>
         </ul>
+    </div>
+    
         <?php include 'layout/footer.php'; ?>
     </body>
 </html>
