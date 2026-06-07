cecf179505bdd7fac279035a1078afd0834a6d40
riem_boukhama <riem.boukhama@studio.unibo.it>
2026-06-07 17:16:52 +0200

    edited admin dashboard for styling

--- template/admin-template.php
+++ template/admin-template.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Area Admin</title>
+    <link rel="stylesheet" href="../css/base.css">
     <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/admin-dashboard.css">
 </head>
 <body>
     <?php include 'layout/header.php'; ?>
     <main>
-        <section>
-            <h2>Statistiche</h2>
+        <h1>Gestione piattaforma</h1>
+        <section class="statistiche">
+            <h2 class="gradient-text">Statistiche</h2>
             <ul>
                 <li>Gruppi creati: <span id="totale-gruppi"><?php echo $adminParams['stats']['totale_gruppi'] ?></span></li>
                 <li>Studenti registrati: <span id="totale-studenti"><?php echo $adminParams['stats']['totale_studenti'] ?></span></li>
@@ -20,8 +23,8 @@
             </ul>
         </section>
 
-        <section>
-            <h2>Gruppi</h2>
+        <section class="gruppi">
+            <h2 class="gradient-text">Gruppi</h2>
             <ul>
                 <?php foreach ($adminParams['groups'] as $g): ?>
                     <li>
@@ -31,61 +34,59 @@
                         Creatore: <?= htmlspecialchars($g['creatore']) ?><br>
                         Partecipanti: <?= $g['partecipanti'] ?><br>
                         Messaggi inviati: <?= $g['messaggi'] ?><br>
-                        <a href="group-details-admin.php?id=<?= $g['id_gruppo'] ?>">Visualizza dettagli</a>             
+                        <a href="group-details.php?id=<?= $g['id_gruppo'] ?>">Visualizza dettagli</a>             
                    </li>
                 <?php endforeach; ?>
             </ul>
         </section>
 
-        <section>
-            <h2>Studenti</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Studente</th>
-                        <th>Email</th>
-                        <th>Stato</th>
-                        <th>Azioni</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($adminParams['users'] as $u): ?>
-                    <tr id="user-<?= $u['id_utente'] ?>">
-                        <td><?= htmlspecialchars($u['nome'] . ' ' . $u['cognome']) ?></td>
-                        <td><?= htmlspecialchars($u['email']) ?></td>
-                        <td class="stato"><?= $u['stato'] ?></td>
-                        <td class="azioni">
-                            <?php if ($u['stato'] === 'attivo'): ?>
-                                <button type="button" onclick="banUser(<?= $u['id_utente'] ?>)">Disattiva</button>
-                            <?php else: ?>
-                                <button type="button" onclick="unbanUser(<?= $u['id_utente'] ?>)">Riattiva</button>
-                            <?php endif; ?>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
-        </section>
-
-        <section>
-            <h2>Messaggi gruppo</h2>
-            <div>
-                <label>Seleziona gruppo:</label>
-                <select id="groupSelect">
-                    <?php foreach ($adminParams['groups'] as $g): ?>
-                        <option value="<?= $g['id_gruppo'] ?>">
-                            <?= htmlspecialchars($g['titolo']) ?>
-                        </option>
+        <div class="sezione-web">
+            <section class="studenti">
+                <h2 class="gradient-text">Studenti</h2>
+                <ul>
+                    <?php foreach ($adminParams['users'] as $u): ?>
+                    <li id="user-<?= $u['id_utente'] ?>">
+                        <div>
+                            <p><?= htmlspecialchars($u['nome'] . ' ' . $u['cognome']) ?></p>
+                            <p><?= htmlspecialchars($u['email']) ?></p>
+                            <p class="stato <?= htmlspecialchars($u['stato']) ?>"><?= $u['stato'] ?></p>
+                        </div>
+                        
+                        <?php if ($u['stato'] === 'attivo'): ?>
+                            <div class="azioni">
+                                <button class="ban btn btn-primary" type="button" onclick="banUser(<?= $u['id_utente'] ?>)">Disattiva</button>
+                            </div>
+                        <?php else: ?>
+                            <div class="azioni">
+                                <button class="unban btn btn-primary" type="button" onclick="unbanUser(<?= $u['id_utente'] ?>)">Riattiva</button>
+                            </div>
+                        <?php endif; ?>
+                    </li>
                     <?php endforeach; ?>
-                </select>
-                <button type="button" onclick="loadMessages()">Visualizza</button>
-            </div>
+                </ul>
+            </section>
 
-            <div id="messages-container">
-                <p>Seleziona un gruppo per vedere i messaggi</p>
-            </div>
-        </section>
+            <section class="messaggi">
+                <h2 class="gradient-text">Messaggi gruppo</h2>
+                <div class="selezione-gruppo">
+                    <label>Seleziona gruppo:</label>
+                    <select id="groupSelect">
+                        <?php foreach ($adminParams['groups'] as $g): ?>
+                            <option value="<?= $g['id_gruppo'] ?>">
+                                <?= htmlspecialchars($g['titolo']) ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                    <button class=" visualizza-messaggi btn btn-primary" type="button" onclick="loadMessages()">Visualizza</button>
+                </div>
+
+                <div id="messages-container">
+                    <p>Seleziona un gruppo per vedere i messaggi</p>
+                </div>
+            </section>
+        </div>
     </main>
+    <?php include 'layout/footer.php'; ?>
     <script src="../js/admin/user-actions.js"></script>
     <script src="../js/admin/group-messages.js"></script>
     <script src="../js/admin/stats.js"></script>
